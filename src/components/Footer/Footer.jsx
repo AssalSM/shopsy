4494c40ec88e3babc152d44df9bfd5d413c5582f
assal <mohammed1assal23@gmail.com>
2024-07-26 03:07:25 +0100

    second commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import footerLogo from "../../assets/logo.png";
 import Banner from "../../assets/website/footer-pattern.jpg";
 import {
@@ -108,11 +108,11 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>ASSAL MOHAMMED</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
-                  <p>+91 123456789</p>
+                  <p>+212 708768573</p>
                 </div>
               </div>
             </div>
